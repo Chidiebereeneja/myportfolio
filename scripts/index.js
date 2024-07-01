c7df1924f669ec6b78d7ab0ea6e5bece40c0ec73
Chidiebere Eneja <chidiebereravy@gmail.com>
2024-07-01 08:33:57 -0700

    Still in progress..

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,11 @@
 "use strict";
+import { typeWriter } from "./about-us.js";
+
+const h1Element = document.querySelector(".h1");
+const textDisplay = `I am glad that you make out time to take a look at my portfolio!`;
+
+typeWriter(h1Element, textDisplay);
 
 setTimeout(function () {
-  window.location.replace("pages/homePage.html");
+	window.location.replace("pages/homePage.html");
 }, 10000);
