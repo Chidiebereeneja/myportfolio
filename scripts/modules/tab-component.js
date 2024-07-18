2559ae6f3d58ea7ca25cb2b643cd33d8a0278364
Chidiebere Eneja <chidiebereravy@gmail.com>
2024-07-18 08:48:17 +0100

    Testing

--- scripts/modules/tab-component.js
+++ scripts/modules/tab-component.js
@@ -6,33 +6,41 @@ const chooseBtnContainer = document.querySelector(".choose--me--header");
 
 function defaultDisplay() {
 	const tabPElement = [...document.querySelector(".choose--me--text").children];
+	const defaultText = document.getElementById("User-Centric");
 
-	tabPElement.forEach((element) => {
-		if (element.id !== "User-Centric") {
-			element.classList.add("display--none");
-		}
+	tabPElement.forEach((element, i) => {
+		element.style.display = "none";
 	});
+
+	defaultText.style.display = "flex";
+	defaultText.style.gap = "1rem";
 }
 
 function tabComponent() {
 	chooseBtnContainer.addEventListener("click", function (e) {
 		e.preventDefault();
+		const textContainer = document.querySelectorAll(".choose--me--text div");
 
 		if (e.target.hasAttribute("href")) {
 			const check = [...e.target.parentElement.children];
 			const targetAttr = e.target.getAttribute("href").slice(1);
 			const targetContent = e.target.textContent;
+
 			const test = check
 				.filter((el) => el.textContent !== targetContent)
-				.map((el) => {
-					const attr = el.getAttribute("href").slice(1);
-					console.log(attr);
+				.map((el) => el.getAttribute("href").slice(1))
+				.map((element) => {
+					const ele = document.getElementById(element);
+					ele.style.display = "none";
 				});
 
 			check.map((el) => {
 				el.classList.remove("active");
 			});
+
 			e.target.classList.add("active");
+			const targetRender = e.target.getAttribute("href").slice(1);
+			document.getElementById(targetRender).style.display = "flex";
 		}
 	});
 }
